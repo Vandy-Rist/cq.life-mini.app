5fb4954fca3ae15bfd6fb38ba99e982099e5a922
Vandy-Rist <lionfan1368@gmail.com>
2020-08-28 18:29:57 +0800

    Initial Commit

---
+++ pages/message/message.js
@@ -0,0 +1,95 @@
+// pages/message/message.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msgList: [
+      {
+        id: '1',
+        title: "食品大促销",
+        date: "2019-12-06",
+        image: "https://m.360buyimg.com/mobilecms/s1125x549_jfs/t14860/139/504394142/191023/ca7c592e/5a2fcd75Ncde0d77d.jpg!q70.jpg",
+        desc: "优惠不要错过"
+      },
+      {
+        id: '2',
+        title: "饮品大促销",
+        date: "2019-12-06",
+        image: "https://m.360buyimg.com/mobilecms/s1125x549_jfs/t14860/139/504394142/191023/ca7c592e/5a2fcd75Ncde0d77d.jpg!q70.jpg",
+        desc: "优惠不要错过"
+      },
+      {
+        id: '3',
+        title: "电商大促销",
+        date: "2019-12-06",
+        image: "https://m.360buyimg.com/mobilecms/s1125x549_jfs/t14860/139/504394142/191023/ca7c592e/5a2fcd75Ncde0d77d.jpg!q70.jpg",
+        desc: "优惠不要错过"
+      },
+      {
+        id: '4',
+        title: "电商大促销",
+        date: "2019-12-06",
+        image: "https://m.360buyimg.com/mobilecms/s1125x549_jfs/t14860/139/504394142/191023/ca7c592e/5a2fcd75Ncde0d77d.jpg!q70.jpg",
+        desc: "优惠不要错过"
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
