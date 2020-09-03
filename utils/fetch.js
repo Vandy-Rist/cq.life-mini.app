19268816c00cc44b7be495c98c22dcaf00e36297
Vandy-Rist <lionfan1368@gmail.com>
2020-09-03 10:26:47 +0800

    test-03

---
+++ utils/fetch.js
@@ -0,0 +1,17 @@
+//封装请求
+module.exports = (url,type,data) => {
+  //返回一个promise对象处理异步逻辑
+  return new Promise((resolve,reject)=>{
+    wx.request({
+      url: url,
+      data: data,
+      header: {
+        "Content-Type": 'json'
+      },
+      method: type,
+      dataType: "json",
+      successs: resolve,
+      fail: reject,
+    })
+  })
+}
